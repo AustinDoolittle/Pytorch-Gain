Fixed scaling issue with transforms

data.py
@@ -23,13 +23,12 @@ def load_image(image_path, resize_dims, expected_channels):
 
     # pytorch is [C, H, W]
     decoded_img = decoded_img.transpose((2, 0, 1))
-    decoded_img = decoded_img.astype(np.float32)
-    decoded_img = decoded_img / 255.0
+    decoded_img = decoded_img.astype(np.float32) / 255.0
     return decoded_img
 
 
 class RawDataset:
-    def __init__(self, root_dir, ds_split=0.8, include_exts=['.jpg', '.png', '.jpeg'], transformer=None, output_dims=(224, 224), output_channels=3, num_workers=1):
+    def __init__(self, root_dir, ds_split=0.8, include_exts=['.jpg', '.png', '.jpeg'], transformer=None, output_dims=(224, 224), output_channels=3, num_workers=1, batch_size_dict=None):
         self.name = os.path.split(root_dir)[-1]
         self._ds_split = ds_split
         self.root_dir = root_dir
@@ -37,6 +36,12 @@ class RawDataset:
         self.include_exts = include_exts
         self.output_dims = output_dims
         self.num_workers = num_workers
+        if not batch_size_dict:
+            batch_size_dict = {
+                'train': 1,
+                'test': 1
+            }
+        self.batch_size_dict = batch_size_dict
 
         # TODO implement transformers
         self.transformer = transformer
@@ -63,7 +68,7 @@ class RawDataset:
                     if not p in image_dict:
                         image_dict[p] = []
 
-                    image_dict[p].append(os.path.join(self.root_dir, p, f))
+                    image_dict[p].append(os.path.join(full_dir, f))
         self.labels = image_dict.keys()
 
         # split the train and test datasets equally among labels
@@ -86,7 +91,7 @@ class RawDataset:
                 ds_args['transformer'] = self.transformer
 
             ds = ImageDataset(filesets['train'], self.labels, **ds_args)
-            ret_dict[k] = torch.utils.data.DataLoader(ds, shuffle=True, num_workers=self.num_workers)
+            ret_dict[k] = torch.utils.data.DataLoader(ds, shuffle=True, num_workers=self.num_workers, batch_size=self.batch_size_dict[k])
 
         return ret_dict
 

gain.py
@@ -400,7 +400,7 @@ class AttentionGAIN:
 
     def _attention_map_forward(self, data, label):
         output_cl = self.model(data)
-        output_cl.backward(gradient=label * output_cl)
+        output_cl.backward(gradient=label)
 
         self.model.zero_grad()
 
@@ -424,12 +424,13 @@ class AttentionGAIN:
         return output_cl, loss_cl, A_c
 
     def _forward(self, data, label):
+        # TODO normalize elsewhere, this feels wrong
         output_cl, loss_cl, A_c = self._attention_map_forward(data, label)
         output_cl_softmax = F.softmax(output_cl, dim=1)
 
         # Eq 4
-        T_A_c = torch.sigmoid(self.omega * (A_c - A_c.mean(dim=2, keepdim=True).mean(dim=1, keepdim=True)))
-        # T_A_c = torch.sigmoid(self.omega * (A_c - self.sigma))
+        # T_A_c = torch.sigmoid(self.omega * (A_c - A_c.mean(dim=2, keepdim=True).mean(dim=1, keepdim=True)))
+        T_A_c = torch.sigmoid(self.omega * (A_c - self.sigma))
 
         # Eq 3
         I_star = data - (T_A_c * data)
@@ -437,8 +438,7 @@ class AttentionGAIN:
         output_am = self.model(I_star)
 
         # Eq 5
-        loss_am = F.softmax(output_am, dim=1) * label
-
+        loss_am = F.sigmoid(output_am) * label
         loss_am = loss_am.sum() / label.sum().type(self.tensor_source.FloatTensor)
 
         # Eq 6

transform.py
@@ -1,7 +1,8 @@
 import imgaug as ia
 import imgaug.augmenters as iaa
+import numpy as np
 
-available_transformers = ['DropoutAndAffine']
+available_transformers = ['Dropout', 'Affine', 'DropoutAndAffine']
 
 
 class TransformerBase:
@@ -14,18 +15,37 @@ class TransformerBase:
 
     def __call__(self, data_dict):
         if not self.transform_labels or data_dict['label/name'] in self.transform_labels:
-            data_dict['image'] = self.augmenter.augment_images(data_dict['image'])
+            image = data_dict['image'] * 255.0
+            image = image.astype(np.uint8)
+            image = self.augmenter.augment_images(image)
+            data_dict['image'] = image.astype(np.float32) / 255.0
 
         return data_dict
 
 
 class DropoutAndAffine(TransformerBase):
     def _build_augmenter(self, **kwargs):
+        affine_xform = Affine()
+        dropout_xform = Dropout()
+        aug = iaa.Sequential([
+           affine_xform.augmenter,
+           dropout_xform.augmenter],
+           random_order=True)
+        return aug
+
+class Affine(TransformerBase):
+    def _build_augmenter(self, **kwargs):
         aug = iaa.SomeOf((0, None), [
-            iaa.Affine(rotate=45),
-            iaa.CoarseDropout((0.01, 0.05), size_percent=(0.05, 0.33)),
+            iaa.Affine(translate_percent=(0, 0.1)),
+            iaa.Affine(rotate=(0, 45)),
             iaa.Fliplr(1),
-            iaa.Flipud(1),
-            iaa.CropAndPad(percent=(-0.25, 0.25))
-        ])
+            iaa.Flipud(1)],
+            random_order=True)
+        return aug
+
+
+class Dropout(TransformerBase):
+    def _build_augmenter(self, **kwargs):
+        aug = iaa.Sometimes(0.5,
+            iaa.CoarseDropout((0.01, 0.05), size_percent=(0.05, 0.25)))
         return aug